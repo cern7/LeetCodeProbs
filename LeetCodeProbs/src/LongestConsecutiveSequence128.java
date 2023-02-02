99c08dbca8d48b25370d48fd715aa94b5a1a0d43
constantin_27@icloud.com <constantin_27@icloud.com>
2023-02-02 08:51:58 +0200

    Solution for ValidPalindrome125 LeetCode

---
+++ LeetCodeProbs/src/LongestConsecutiveSequence128.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class LongestConsecutiveSequence128 {
+    public static int longestConsecutive(int[] nums) {
+        int size = nums.length;
+        if (size == 0) {
+            return 0;
+        }
+        if (size == 1) {
+            return 1;
+        }
+        int length = 0;
+
+        Set<Integer> neigbhours = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+
+        for (int i = 0; i < nums.length; i++) {
+            int number = nums[i];
+            int tempLength = 1;
+            if (!neigbhours.contains(number - 1)) {
+                while (neigbhours.contains(number + tempLength)) {
+                    tempLength++;
+                }
+                length = length < tempLength ? tempLength : length;
+            }
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        System.out.println(longestConsecutive(nums));
+    }
+}
---
+++ LeetCodeProbs/src/ValidPalindrome125.java
@@ -0,0 +1,42 @@
+public class ValidPalindrome125 {
+    public static void main(String[] args) {
+        String s = " ";
+        System.out.println(new ValidPalindrome125().isPalindrome(s));
+    }
+
+
+    public boolean isPalindrome(String s) {
+        int size = s.length();
+        if (size <= 1) {
+            return true;
+        }
+        int left = 0;
+        int right = size - 1;
+
+        while (left < right) {
+            char leftC = s.charAt(left);
+            char rightC = s.charAt(right);
+            if (!Character.isLetterOrDigit(leftC)) {
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(rightC)) {
+                right--;
+                continue;
+            }
+            if (Character.isLetter(leftC)) {
+                leftC = Character.toLowerCase(leftC);
+            }
+            if (Character.isLetter(rightC)) {
+                rightC = Character.toLowerCase(rightC);
+            }
+            if (leftC != rightC) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
