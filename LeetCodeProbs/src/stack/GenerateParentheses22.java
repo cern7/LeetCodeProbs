aea3166367d94bd3e22c17234f6438cfb02987fb
constantin_27@icloud.com <constantin_27@icloud.com>
2023-02-07 21:43:47 +0200

    use StringBuilder as stack, stop recursion when add string to result

--- LeetCodeProbs/src/stack/GenerateParentheses22.java
+++ LeetCodeProbs/src/stack/GenerateParentheses22.java
@@ -5,28 +5,31 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GenerateParentheses22 {
-   
+
     public List<String> generateParenthesis(int n) {
         if (n == 1) {
             return Arrays.asList("()");
         }
         List<String> result = new ArrayList<>();
-        helper(n, 0, 0, "", result);
+        helper(n, 0, 0, new StringBuilder(""), result);
         return result;
     }
 
-    private void helper(int n, int open, int closed, String parentheses, List<String> set) {
+    private void helper(int n, int open, int closed, StringBuilder parentheses, List<String> set) {
         // add open parenth if open < n
-        // add closing parenth if closed < open
+        // add closing paranth if closed < open
         // valid if open == closed == n
         if (open == closed && open == n) {
-            set.add(parentheses);
+            set.add(parentheses.toString());
+            return;
         }
         if (open < n) {
-            helper(n, open + 1, closed, parentheses + "(", set);
+            helper(n, open + 1, closed, parentheses.append("("), set);
+            parentheses.deleteCharAt(parentheses.length() - 1);
         }
         if (closed < open) {
-            helper(n, open, closed + 1, parentheses + ")", set);
+            helper(n, open, closed + 1, parentheses.append(")"), set);
+            parentheses.deleteCharAt(parentheses.length() - 1);
         }
     }
 }
